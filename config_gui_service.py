baf42c1e86f1732607b94a49a234da19d535a417
Iwona Sasak <sasak.iwona.88@gmail.com>
2017-08-11 14:31:35 +0200

    fixing browsing and setting entries on grid

--- config_gui_service.py
+++ config_gui_service.py
@@ -9,7 +9,7 @@ import re
 def present_user_config_form():
     root=tk.Tk()
     root.title("Configure copy/rename script")
-    root.geometry("600x200")
+    root.geometry("550x100")
     add_grid(root)
     root.mainloop()
     
@@ -19,23 +19,31 @@ def quit_win(root):
 def add_grid(root):
     add_labels(root)
 
-    e1 = tk.Entry().grid(row=0, column=1)
-    e2 = tk.Entry().grid(row=1, column=1)
-
-    tk.Button(root, text="Browse", command= lambda: browse(e1)).grid(row=0, column=2)
-    tk.Button(root, text="Browse", command= lambda: browse(e2)).grid(row=1, column=2)
-
+    e1 = tk.Entry(width=50)
+    e2 = tk.Entry(width=50)
+    e1.grid(row=0, column=1)
+    e2.grid(row=1, column=1)
+    
+    tk.Button(root, text="Browse", command= lambda: browse_file(e1)).grid(row=0, column=2)
+    tk.Button(root, text="Browse", command= lambda: browse_folder(e2)).grid(row=1, column=2)
+    
     tk.Button(root, text='Generate config file', command= lambda: generate_config(root, e1.get(), e2.get())).grid(row=3, column=0, sticky=tk.W, pady=4)
-    tk.Button(root,text="Close", command= lambda: quit_win(root)).grid(row=3, column=1, sticky=tk.W, pady=4)
+    tk.Button(root, text="Close", command= lambda: quit_win(root)).grid(row=3, column=1, sticky=tk.W, pady=4)
 
 def add_labels(root):
     tk.Label(root, text="File to be copied").grid(row=0)
     tk.Label(root, text="Destination directory").grid(row=1)
 
-def browse(entry):
+def browse_file(entry):
     directory = filedialog.askopenfilename()
-    entry.config(text=directory)
-
+    entry.insert(0,directory)
+    print(entry.get())
+    
+def browse_folder(entry):
+    directory = filedialog.askdirectory()
+    entry.insert(0,directory)
+    print(entry.get())
+    
 def generate_config(root, source_path, destination_path):
     file_regex = '[\w\-. ]+.html$'
     file_name = re.match(file_regex, source_path)[0]
